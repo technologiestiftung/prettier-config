542ba5e1d799771dc3583d010e92c75cce94d4d2
ff6347 <hash@ff6347.email>
2024-03-15 13:38:51 +0100

    chore(init): Setup repo

---
+++ index.js
@@ -0,0 +1,16 @@
+export default {
+	tabWidth: 2,
+	semi: true,
+	singleQuote: false,
+	useTabs: true,
+	trailingComma: "all",
+	printWidth: 80,
+	overrides: [
+		{
+			files: ["*.yml", "*.yaml"],
+			options: {
+				useTabs: false,
+			},
+		},
+	],
+};
---
+++ test/sample.js
@@ -0,0 +1,4 @@
+const foo = {
+	arr: [1, 2, 3],
+	quotes: "single",
+};
